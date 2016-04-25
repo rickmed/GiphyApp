f5d06077149a99d47c11fc19e03b86519ca670ec
ricmed205 <rickmedina85@gmail.com>
2016-04-25 12:54:04 -0430

    deleted duplicated folder

--- gifApp/gifApp/utils/data.js
+++
@@ -1,8 +0,0 @@
-// export const dummyQueries = ['rihana singing in grammy 2016 good']
-export const dummyQueries = ['dsadsdsadsadDPDSA', 'rihana singing in grammy 2016 good', 'iphone', 'tree', 'EPALEPALEEPAL', 'buildings', 'horses', 'car', 'shoe']
-
-export const randomGifBase = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='
-
-export const respImgPath = ['body', 'data', 'image_original_url'] // for randomGifBase
-
-export const colors = ['red', 'orange', 'yellow', 'olive', 'green', 'teal', 'violet', 'purple', 'pink', 'black', 'brown']
