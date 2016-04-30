84d7ba2b1f572ddf9eaca0d19eb72f2c1c9d0229
Ricardo Medina <rickmedina85@gmail.com>
2016-04-30 16:14:32 -0430

    update

--- gifApp/components/Card.jsx
+++ gifApp/components/Card.jsx
@@ -1,4 +1,4 @@
-itimport React from 'react'
+import React from 'react'
 import {observer} from 'mobx-react'
 import {observable} from 'mobx'
 import {Observable as Observable$} from 'rxjs/Observable'
